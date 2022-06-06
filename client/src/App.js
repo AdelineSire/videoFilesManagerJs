d6a144032a59434b15990afbc71e6a289219299a
AdelineSire <65898303+AdelineSire@users.noreply.github.com>
2022-06-06 19:54:37 +0200

    Adds player

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 
 import FileUpload from './components/FileUpload';
 import FilesList from './components/FilesList';
-import axios from 'axios';
-
-const apiUrl = process.env.API_URL || 'http://localhost:3002';
+import Player from './components/Player';
 
 const App = () => {
-	const [filesList, setFilesList] = useState([]);
-
-	const getFiles = async () => {
-		const response = await axios.get(`${apiUrl}/files`);
-		console.log('response.data', response.data);
-		return response.data;
-	};
-
-	useEffect(() => {
-		getFiles().then((files) => {
-			setFilesList(files);
-			console.log('files in useEffect', filesList);
-		});
-	}, []);
+	const [selectedFile, setSelectedFile] = useState(null);
+	const [uploaded, setUploaded] = useState(false);
+	console.log('selectedFile in app', selectedFile);
 
 	return (
-		<Container>
-			<Row>
-				<h4>File Upload</h4>
-				<FileUpload />
-				<FilesList files={filesList} />
-			</Row>
+		<Container className='text-center'>
+			<h1 className='m-5'>Video Manager</h1>
+			<FileUpload setUploaded={setUploaded} uploaded={uploaded} />
+			<FilesList setSelectedFile={setSelectedFile} uploaded={uploaded} />
+			{selectedFile && <Player file={selectedFile} />}
 		</Container>
 	);
 };
