26256f938bcbbafd7afafc7736e9f827f4732bcf
AdelineSire <65898303+AdelineSire@users.noreply.github.com>
2022-06-06 19:49:26 +0200

    Adds FilesList

--- client/src/components/FilesList.jsx
+++ client/src/components/FilesList.jsx
@@ -1,13 +1,34 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Row from 'react-bootstrap/Row';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const FilesList = ({ files }) => {
+const FilesList = ({ setSelectedFile, uploaded }) => {
+	const [filesList, setFilesList] = useState([]);
+	const apiUrl = process.env.API_URL || 'http://localhost:3002';
+
+	const getFiles = async () => {
+		const response = await axios.get(`${apiUrl}/files`);
+		console.log('response.data', response.data);
+		setFilesList(response.data);
+	};
+
+	useEffect(() => {
+		getFiles();
+	}, [uploaded]);
+
 	return (
-		<Row>
-			<ListGroup defaultActiveKey='none'>
-				{files.map((file) => (
-					<ListGroup.Item action onClick={() => console.log('file', file)}>
-						{file}
+		<Row className='mb-5 d-flex flex-column align-items-center'>
+			<h2 className='mb-4'>Lire une vidéo</h2>
+			<ListGroup>
+				{filesList.map((file) => (
+					<ListGroup.Item
+						key={file.fileName}
+						action
+						onClick={() => setSelectedFile(file)}
+					>
+						{file.fileName}
 					</ListGroup.Item>
 				))}
 			</ListGroup>
